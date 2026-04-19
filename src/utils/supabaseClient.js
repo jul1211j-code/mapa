c44664ed5c7ae9ce38aabe00d0685c444116458b
jul1211j-code <jul.1211j@gmail.com>
2026-04-18 18:00:17 -0600

    Update Supabase key to use publishable key

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://kwfhfpkqfwusahrsqvms.supabase.co' // <-- pega aquí
-const supabaseKey = 'kwfhfpkqfwusahrsqvms' // <-- pega aquí
+const supabaseKey = 'sb_publishable_TeQXSLx7kYsYcqzQ7D8TNQ_Y7D80f34' // <-- pega aquí
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
